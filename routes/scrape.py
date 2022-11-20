dd5cea12606475e2313509e442a5a7a03e35ead0
BishalRaj <bisal1shakya@gmail.com>
2022-11-20 09:48:12 +0000

    url changed and model added

--- routes/scrape.py
+++ routes/scrape.py
@@ -1,29 +1,51 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
 from controller.scraper.ups_us import fetch
-from controller.scraper.multiplePage import getAllData
-
+from controller.scraper.multiplePage import getProductData, getSingleProduct
 scrapeRouter = APIRouter()
 
 
+class Item(BaseModel):
+    url: str
+
+
+class SaveItem(BaseModel):
+    token: str
+    pid: str
+
+
 @scrapeRouter.get('/scrape/ups/{state}')
-async def fetchALl(state: str):
+def fetchALl(state: str):
     if fetch(state):
         return "success"
     else:
         return "failed"
 
 
-@scrapeRouter.get('/scrape/amazon/{keyword}')
-async def fetchALl(keyword: str):
-    if getAllData(keyword):
+@scrapeRouter.get('/scrape/etzy/{keyword}')
+def fetchALl(keyword: str):
+    if getProductData(keyword):
         return "success"
     else:
         return "failed"
 
 
+@scrapeRouter.post('/scrape/etzy/single')
+def fetchSingle(data: Item):
+    # print(url)
+    response = getSingleProduct(data.url)
+    if response:
+        response = response[0]
+    return response
+
+
+@scrapeRouter.post('/scrape/etzy/single/save')
+def saveSingle(data: SaveItem):
+    print(data.token)
+
+
 @scrapeRouter.get('/scrape/amazon/all/{keyword}')
-async def fetchAll(keyword: str):
+def fetchAll(keyword: str):
     if keyword[-1] == " ":
         keyword = keyword.rstrip(keyword[-1])
     return keyword
